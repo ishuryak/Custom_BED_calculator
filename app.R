ba742c9ddc44d151d908f53a7072eb992b0b9e5b
Igor Shuryak <ishuryak@gmail.com>
2024-10-25 16:01:59 -0400

    Updated app.R with user-defined parameters for BED Calculator

--- app.R
+++ app.R
@@ -1,17 +1,21 @@
-
-
 library(shiny)
 library(ggplot2)
 
 # Define UI
 ui <- fluidPage(
-  titlePanel("BED Calculator"),
+  titlePanel("Advanced BED Calculator"),
   
   sidebarLayout(
     sidebarPanel(
       numericInput("m", "Number of dose fractions (m):", 30, min = 1),
       numericInput("d", "Dose/fraction (d) in Gy:", 2, min = 0),
-      numericInput("T", "Total treatment time (T) in days:", 42, min = 1)
+      numericInput("T", "Total treatment time (T) in days:", 42, min = 1),
+      hr(),
+      numericInput("alpha", "Linear tumor cell killing parameter (α) in 1/Gy:", 0.2, min = 0, step = 0.01),
+      numericInput("r", "Alpha/beta ratio (α/β) in Gy:", 10, min = 0, step = 0.1),
+      numericInput("L_DI", "Accelerated repopulation rate (DI model):", 0.2, min = 0, step = 0.01),
+      numericInput("L_DD", "Max repopulation rate (DD model):", 0.5, min = 0, step = 0.01),
+      numericInput("Tk", "Time to accelerated repopulation (days):", 28, min = 0, step = 1)
     ),
     
     mainPanel(
@@ -24,18 +28,16 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output) {
   
-  # Parameters
-  alpha <- 0.2
-  r <- 10
-  L_DI <- 0.2
-  L_DD <- 0.5
-  Tk <- 28
-  
   # Reactive expression for calculations
   bed_calcs <- reactive({
     m <- input$m
     d <- input$d
     T <- input$T
+    alpha <- input$alpha
+    r <- input$r
+    L_DI <- input$L_DI
+    L_DD <- input$L_DD
+    Tk <- input$Tk
     
     BED_DI <- m*d*(1 + d/r) - L_DI*log(1 + exp(3*T - 3*Tk))/(3*alpha)
     
